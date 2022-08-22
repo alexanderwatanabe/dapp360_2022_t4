c9a89bb632bc188c5d8359f227580926210df701
Alexander Watanabe <alex.watanabe@gmail.com>
2022-08-22 17:36:44 +0100

    added BIP39 list and a function to load it

--- Main.hs
+++ Main.hs
@@ -1,36 +1,43 @@
-module Main where
+
+import System.IO
+import Control.Monad
+
+
 
 --TYPES
-data BipWord = BipWord Int String
-
-data BipList = [BipWord]
-
---List of shuffled indices
-data BipOrder = [Int]
-
-
---BIP LIST/ORDERS
+data BipWord = BipWord String
 
 --Build a BipList from a file
-fileToBipList :: IO () -> BipList
+fileToBipList :: IO () -> [BipWord]
+fileToBipList = undefined
 
 --Take an int, return a shuffled list of 2048 BIP39 keywords
-shuffleBIP :: [Int] -> BipOrder
+shuffleBIP :: [Int] -> [Int]
+shuffleBIP = undefined
+
+loadBIP :: FilePath -> IO [String]
+loadBIP path = do
+    contents <- readFile path
+    return (lines contents)
 
 
 --TUI
 
 --take a BipWord, BipList and BipOrder, return the Int of that Word
-wordToInt :: BipWord -> BipList -> BipOrder -> Int
+wordToInt :: BipWord -> [BipWord] -> [Int] -> Int
+wordToInt = undefined
 
 --take an Int, BipList and BipOrder, return the appropriate BipWord
-intToWord :: Int -> BipList -> BipOrder -> BipWord
+intToWord :: Int -> [BipWord] -> [Int] -> BipWord
+intToWord = undefined
 
 --Take a BIP list and a shuffle and turn to a 2x2 matrix
-buildShuffle :: BipList -> BipOrder -> [[BipWord]]
+buildShuffle :: [BipWord] -> [Int] -> [[BipWord]]
+buildShuffle = undefined
 
 --2x2 Matrix of BipWords and show on screen
-displayShuffle:: [[BipWord]] -> IO ()
+displayShuffle :: [[BipWord]] -> IO ()
+displayShuffle = undefined
 
 
 --SELECTING AND TOGGLING CELLS FOR INCLUSION
@@ -41,4 +48,7 @@ displayShuffle:: [[BipWord]] -> IO ()
 
 --EXPORTING
 
-main :: IO ()
+
+main = do  
+    contents <- readFile "bip39_english.txt"
+    print . words $ contents
