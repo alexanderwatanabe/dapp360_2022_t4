a86f0273dff075bd2369caa7ed432415b56f363f
Alexander Watanabe <alex.watanabe@gmail.com>
2022-08-24 02:36:28 +0100

    changed display to [[Int]], polymorphic type and added test inputs

--- src/Main.hs
+++ src/Main.hs
@@ -9,7 +9,7 @@ import Brick.Widgets.Table
     ( renderTable, surroundingBorder, table, Table )
 import Brick.Widgets.Center (center)
 import Bip ( bip39words )
-import qualified Data.Text as Text
+import qualified Data.Text as T
 
 
 
@@ -20,14 +20,20 @@ newtype BipList  = BipList [(Int, String)] deriving (Eq, Show)
 bip39 :: BipList
 bip39 = BipList $ zip [1..2048] bip39words
 
-selectedStats :: [[Bool]]
-selectedStats = take 46 $ repeat . take 46 $ repeat False
 
-textifyNestedList :: Show a => [[a]] -> [[Text.Text]]
-textifyNestedList a = Prelude.map (Prelude.map (Text.pack . (\x -> if x == "False" then "0" else "1") . show) ) a
+--Take a list of Ints
+twoDify :: [Int] -> [[Int]]
+twoDify []     = []
+twoDify (x:xs) = map (+ x) [0..45] : twoDify xs
 
-widgetizeNestedList :: [[Text.Text]] -> [[Widget ()]]
-widgetizeNestedList a = Prelude.map (Prelude.map txt ) a
+        
+
+
+textifyNestedList :: Show a => [[a]] -> [[T.Text]]
+textifyNestedList = map ( map (T.pack . show) )
+
+widgetizeNestedList :: [[T.Text]] -> [[Widget ()]]
+widgetizeNestedList = map ( map txt )
 
 --For future, load custom BIP list
 loadBIP :: FilePath -> IO [String]
@@ -44,13 +50,12 @@ shuffleBIP r (BipList bl) =  BipList $ shuffle' bl 2048 (mkStdGen r)
 
 --Take a [Int], apply them in order as seeds for shuffling a BipOrder
 listShuffleBIP :: [Int] -> BipList -> BipList
-listShuffleBIP []     bl = bl
-listShuffleBIP (r:rs) bl = listShuffleBIP rs $ shuffleBIP r bl
+listShuffleBIP rs bl = foldl (flip shuffleBIP) bl rs
 
 --2x2 Matrix of BipWords and show on screen
 twoByTwoBIP :: BipList -> [[(Int, String)]]
 twoByTwoBIP (BipList []) = [[(0,"")]]
-twoByTwoBIP (BipList bl) = (take 46 bl) : (twoByTwoBIP $ (BipList (drop 46 bl)))
+twoByTwoBIP (BipList bl) = take 46 bl : (twoByTwoBIP $ BipList (drop 46 bl))
 
 
 --RECOVER SEED PHRASE
@@ -71,19 +76,10 @@ recover (w:ws) bools = undefined
 --Prompt for 12, 15, 24 word seed phrase
 --Check for correct number of words
 
-fakeTableData :: [[Widget ()]]
-fakeTableData = [ [txt "a", txt "b", txt "c"]
-                , [txt "d", txt "e", txt "f"]
-                , [txt "g", txt "h", txt "i"]
-                ]
-
---fakeTableData' :: [[Widget ()]]
---fakeTableData' = map txt twoByTwoBIP bip39
-
 t :: Table ()
 t =
     surroundingBorder True $
-    table . widgetizeNestedList $ textifyNestedList selectedStats
+    table . widgetizeNestedList . textifyNestedList $ twoDify [1,46..2048]
 
 ui :: Widget ()
 ui = center $ renderTable t
